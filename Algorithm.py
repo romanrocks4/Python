103714e2880dbcb746880ed0ef69c49ac5ea9117
romanrocks4 <roman.shubin27@gmail.com>
2023-10-31 16:47:22 -0400

    This is the complited version of the algorithm.

---
+++ Algorithm.py
@@ -0,0 +1,77 @@
+import random
+
+# This is creating the bounderies for the number guessing game
+Lower = int(input("Enter lowest number to guess "))
+Higher = int(input("Enter highest number to guess "))
+
+# This is establishing the variables
+Guess = 0
+Counter = 0
+Highscore_list = {}
+
+Game = True
+
+def add_user(Name, Counter):
+    
+    #Highscore_list.update({"Name": Name, "Counter": Counter})
+
+    Highscore_list[Name] = Counter
+
+
+
+
+while Game:
+    # This is generating the the random number and printing the menu to choose from
+    Number = random.randint(Lower, Higher)
+    print("Romans number game!!")
+    print("1.Start")
+    print("2.See highscores")
+    print("3.Exit")
+
+    Choice = int(input("Enter yout choice:"))
+
+    if Choice == 1:
+        # This is looping the game until the player guesses correct
+        while ( Number != Guess):
+
+           
+            Guess = int(input("Enter your guess :"))
+           # This is making sure the player does not guess outside of the number bounderies
+            if Guess < Lower :
+                print ("Please enter a number within the range")
+
+            elif Guess > Higher :
+                print ("Please enter number within the range")
+
+            # This tells the player that their guess is incorrect and tells them if it is lower or higher than the correct guess
+            elif Guess < Number :
+                print ("your guess is too low")
+                Counter = Counter + 1
+
+            elif Guess > Number :
+                print ("you guess is too high")
+                Counter = Counter + 1
+
+       # this tells the player that they have guessed correctly and resets the guess and counter to 0
+        Counter = Counter + 1
+        print ("Congrats!! you did it in", Counter,"tries!!")
+        
+        Name = str(input("Enter your name:"))
+        add_user(Name, Counter)
+
+        Guess = 0
+        Counter = 0
+        Name = 0
+
+    elif Choice == 2:
+
+        print (Highscore_list)
+
+    # this ends the program
+    elif Choice == 3:
+        print ("Goodbye!!") 
+        Game = False
+
+    #this catches any invalid choices
+    else:
+        print("invaild! Pick 1 2 or 3")
